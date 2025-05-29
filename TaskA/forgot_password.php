5544e5eef8d452e859a4acbbbb1f5497c2f4f510
Glasswatch <flamingrat03@gmail.com>
2025-05-29 11:32:14 +0800

    fixed the otp css

--- TaskA/forgot_password.php
+++ TaskA/forgot_password.php
@@ -50,8 +50,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 <!-- forgot_password.php UI -->
-<form method="POST">
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="register_design.css">
+ </head>
+ <body>
+
+ <div class="container">
+        <div class="bir-header">
+           <img src="../taskb/picture.png" alt="BIR Logo" class="bir-img">
+            <h2>BIR</h2>
+        </div> 
+
+    <div>
+  <form method="POST">
     <h2>Forgot Password</h2>
     <input type="email" name="email" placeholder="Enter your registered email" required><br><br>
     <button type="submit">Send OTP</button>
 </form>
+</div>
+ </body>
+ </html>
+ 
--- TaskA/verify_otp.php
+++ TaskA/verify_otp.php
@@ -23,8 +23,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 <!-- verify_otp.php UI -->
-<form method="POST">
-    <h2>Verify OTP</h2>
-    <input type="text" name="otp" placeholder="Enter OTP" required><br><br>
-    <button type="submit">Verify</button>
-</form>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="register_design.css">
+ </head>
+ <body>
+
+ <div class="container">
+        <div class="bir-header">
+           <img src="../taskb/picture.png" alt="BIR Logo" class="bir-img">
+            <h2>BIR</h2>
+        </div> 
+
+    <div>
+    <form method="POST">
+        <h2>Verify OTP</h2>
+        <input type="text" name="otp" placeholder="Enter OTP" required><br><br>
+        <button type="submit">Verify</button>
+    </form>
+</div>
+ </body>
+ </html>
+ 
